f17ca3889798bf9d4cb8dfb6a85392c773946c2a
khaihc <hoangkhaikute1508@gmail.com>
2022-09-21 14:39:59 +0700

    done setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ButtonModule} from 'primeng/button';
+import { CardModule, } from 'primeng/card';
+
+import {MenubarModule} from 'primeng/menubar';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    CardModule,
+    MenubarModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
